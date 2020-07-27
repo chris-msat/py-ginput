5b56292c1885434dbf9eadaf0be7ba384482cc6a
Joshua Laughner <jlaugh@caltech.edu>
2020-07-27 10:12:47 -0700

    Bump version to 1.0.8 which merges sat interface changes

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     desciption='Python code that creates the .mod and .vmr files used in GGG',
     author='Joshua Laughner, Sebastien Roche, Matthaeus Kiel',
     author_email='jlaugh@caltech.edu',
-    version='1.0.7',  # make sure stays in sync with the version in ginput/__init__.py
+    version='1.0.8',  # make sure stays in sync with the version in ginput/__init__.py
     url='',
     install_requires=[
         'astropy>=3.1.2',
