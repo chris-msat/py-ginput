7e4058307707db8712d4646ed4f3d8a0b2b02537
Joshua Laughner <jlaugh@caltech.edu>
2021-08-06 18:09:33 -0700

    Test files updated with H2O supersaturation fix.

--- ginput/testing/showdiff.sh
+++ ginput/testing/showdiff.sh
@@ -12,7 +12,7 @@ hr="$2"
 
 if [[ $mod_or_vmr == "mod" ]]; then
     dir="mod_files/fpit/oc/vertical"
-    fname="FPIT_20180101_${hr}00Z_36.60N_97.49W.mod"
+    fname="FPIT_20180101${hr}Z_37N_097W.mod"
 elif [[ $mod_or_vmr == "vmr" ]]; then
     dir="vmr_files/fpit/"
     fname="JL1_20180101${hr}Z_37N_097W.vmr"
@@ -28,4 +28,6 @@ fi
 file_in="test_input_data/$dir/$fname"
 file_out="test_output_data/$dir/$fname"
 
+#echo $file_in
+#echo $file_out
 vimdiff "$file_in" "$file_out"
